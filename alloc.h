4ec7c25344849537d47cd94a54a70db9f0dce22e
Donggggg <ptsaturn68@daum.net>
2020-11-05 14:49:00 +0900

    complete to make simple memory manager

--- alloc.h
+++ alloc.h
@@ -7,23 +7,21 @@
 #define PAGESIZE 4096 //size of memory to allocate from OS
 #define MINALLOC 8 //allocations will be 8 bytes or multiples of it
 
-
 typedef struct _memory {
 	int offset; // 메모리 주소 시작 위치
 	int size; // 할당된 크기
 	char* data; // 데이터 정보
-	struct _memory* next;
+	struct _memory* next; // 다음 메모리 포인터
 } Memory;
 
-typedef struct memory_controller {
+typedef struct memory_manager {
 	int total_size; // 사용가능한 메모리 크기
-	int alloc_num; // 할당된 메모리 덩어리 개수
-	int chunk_num; // 할당되지 않은 메모리 덩어리 개수 
-	Memory* alloc_head;
-	Memory* alloc_tail;
-	Memory* chunk_head;
-	Memory* chunk_tail;
-} Controller;
+	char *page; // page 포인터
+	Memory* alloc_head; // alloc의 head 포인터
+	Memory* alloc_tail; // alloc의 tail 포인터
+	Memory* chunk_head; // chunk의 head 포인터
+	Memory* chunk_tail; // chunk의 tail 포인터
+} Manager;
 
 // function declarations
 void print_alloc();
